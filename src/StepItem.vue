1407d6b3ce281267b6d8021cb645b1cb078a82ff
balasukanta <32995019+balasukanta@users.noreply.github.com>
2017-10-23 16:46:36 +0600

    custom progress tracker added

--- src/StepItem.vue
+++ src/StepItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <li class="progress-step" :class="{ 'is-active': isActive, 'is-complete': isComplete }">
-    <span class="progress-marker">{{ marker }}</span>
-    <span class="progress-text" v-if="title">
+  <li v-on:mouseover="myHover" class="progress-step" :class="{'is-active': isActive, 'is-complete': isComplete }">
+    <span :class="{myCustomClass: myCustomClass, tooltip: tooltip, 'is-tooltip-top': tooltipTop}" v-bind:data-tooltip="data" class="progress-marker">{{ marker }}</span>
+    <span  class="progress-text" v-if="title">
       <h4 class="progress-title">{{ title }}</h4>{{ text }}
       <slot></slot>
     </span>
@@ -9,7 +9,16 @@
 </template>
 
 <script>
+import '../../../client/assets/scss/tooltip.scss'
 export default {
+  data () {
+    return {
+      data: '',
+      tooltip: false,
+      tooltipTop: false,
+      myCustomClass: false
+    }
+  },
   props: {
     isActive: Boolean,
     isComplete: Boolean,
@@ -21,16 +30,50 @@ export default {
     title: String,
     text: String
   },
-
-  created () {
-    this._isStepItem = true
-  },
-
   computed: {
     index () {
       return this.$parent.$stepItems.indexOf(this)
     }
+  },
+  created () {
+    this._isStepItem = true
+  },
+  mounted () {
+    this.myMethod ()
+  },
+  methods: {
+    myMethod () {
+      if(this.$parent.$stepItems.indexOf(this) === 0) {
+        this.myCustomClass = true
+      }
+    },
+    myHover () {
+      this.tooltip = true
+      this.tooltipTop = true
+      if(this.$parent.$stepItems.indexOf(this) === 0) {
+        this.tooltip = false
+        this.tooltipTop = false
+        this.data = 'Mobilise'
+      }
+      if(this.$parent.$stepItems.indexOf(this) === 1) {
+        this.data = 'Assess'
+      }
+      if(this.$parent.$stepItems.indexOf(this) === 2) {
+        this.data = 'Design'
+      }
+      if(this.$parent.$stepItems.indexOf(this) === 3) {
+        this.data = 'Implement'
+      }
+      if(this.$parent.$stepItems.indexOf(this) === 4) {
+        this.data = 'Sustain'
+      }
+    }
   }
-
 }
 </script>
+<style>
+.myCustomClass {
+  width:70px!important;
+  height:30px!important;
+}
+</style>
